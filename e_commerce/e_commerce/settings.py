7a0a6eb7bfba91fcfe638d36a759ff4a951938e0
leo2op <midhunfcb@gmail.com>
2023-05-22 16:25:50 +0530

    commit

--- e_commerce/e_commerce/settings.py
+++ e_commerce/e_commerce/settings.py
@@ -79,24 +79,25 @@ WSGI_APPLICATION = 'e_commerce.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
+import os
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
 
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': (BASE_DIR/'db.sqlite3'),
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'e_commerce',
+#         'USER': 'root',
+#         'PASSWORD': '',
+#         'HOST': 'localhost',  # Typically 'localhost' or '127.0.0.1'
+#         'PORT': '3306',  # Typically '3306'
 #     }
 # }
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'e_commerce',
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': 'localhost',  # Typically 'localhost' or '127.0.0.1'
-        'PORT': '3306',  # Typically '3306'
-    }
-}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
